1b6d1594333bb822cfa539341fc4091eb45243ba
amcabee3 <amcabee3@gatech.edu>
2017-11-06 08:29:06 -0500

    Updated version of dist

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mitreapi',
-    version='1.0',
+    version='1.1',
     description='MITRE Attack API',
     author=['Anna McAbee','Roberto Rodriguez'],
     author_email=['annamcabee@gmail.com','rrodri0622@gmail.com'],
